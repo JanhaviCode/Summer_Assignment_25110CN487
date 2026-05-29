654db78166430d3c301199026de582bd9d1f578d
JanhaviCode <janhaviiee97@gmail.com>
2026-05-29 22:51:58 +0530

    Day-3 uplodaded.

---
+++ Day-3/11.c
@@ -0,0 +1,13 @@
+//Write a program to find GCD of two numbers.
+#include <stdio.h>
+int main() {
+    int a, b, i, gcd = 1;
+    printf ("Enter the two numbers=");
+    scanf("%d%d", &a, &b);
+    for(i = 1; i <= a && i <= b; i++) {
+        if(a % i == 0 && b % i == 0)
+            gcd = i;
+    }
+    printf("GCD of %d and %d is %d", a,b, gcd);
+    return 0;
+}
---
+++ Day-3/12.c
@@ -0,0 +1,17 @@
+// WAP to find the LCM of two digits.
+#include <stdio.h>
+int main() {
+    int a, b, max, lcm;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    max = (a > b) ? a : b;
+    while(1) {
+        if(max % a == 0 && max % b == 0) {
+            lcm = max;
+            break;
+        }
+        max++;
+    }
+    printf("LCM of %d and %d is %d", a, b, lcm);
+    return 0;
+}
